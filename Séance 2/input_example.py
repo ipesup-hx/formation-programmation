372d98f3cb31787ff9eb7878b13659d164e31628
Laurent Attias <laurent.attias@m4x.org>
2025-02-27 21:33:29 +0100

    ajout d'éléments pour la séance 2

---
+++ Séance 2/input_example.py
@@ -0,0 +1,11 @@
+print("Addition de deux nombres entiers")
+
+a = input("Entrez un premier nombre : ")
+
+b = input("Entrez un deuxième nombre : ")
+
+a = int(a)
+
+b = int(b)
+
+print(f"{a}+{b}={a+b}")
